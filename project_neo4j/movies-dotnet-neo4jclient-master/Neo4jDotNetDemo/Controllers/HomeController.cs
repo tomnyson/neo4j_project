5491723453b7e5e52d0d4b3e6f22ad098cc8df75
tomnyson <tabletkindfire@gmail.com>
2017-04-17 20:50:50 +0700

    add project and database
    project run dot net mvc neet upgrade some feature
    database neo4j clone to sample data

---
+++ project_neo4j/movies-dotnet-neo4jclient-master/Neo4jDotNetDemo/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿namespace Neo4jDotNetDemo.Controllers
+{
+    using System.Web.Mvc;
+
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ project_neo4j/movies-dotnet-neo4jclient-master/Neo4jDotNetDemo/Controllers/TestController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Neo4jDotNetDemo.Models;
+namespace Neo4jDotNetDemo.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        Person ps = new Person();
+        public ActionResult Index()
+        {
+            ps.Id = 12;
+            ps.Name = "just test";
+            var students = new List<string>
+            {
+                "A",
+                "B",
+                "C"
+            };
+
+            //string name = "hello world";
+            ViewBag.students = students;
+            ViewBag["nguoi"] = ps;
+            return View(ps);
+        }
+    }
+}
